c6fb88ea3b599ffae7cf3d80d093c0cf332ce14d
nurlailathw-jpg <nurlaila.thw@gmail.com>
2026-07-22 12:44:49 +0700

    Create r_programming_102.r

---
+++ r_programming_102.r
@@ -0,0 +1,62 @@
+```R
+# Five core principles
+# 1. variable
+# 2. data types
+# 3. data structure
+# 4. function
+# 5. control flow
+
+Shortcuts, working with R 
+CTRL + ENTER = run code
+CTRL + L = clear console 
+CTRL + shift +  s = Source file, run upper to under (All)
+
+  Highlight lines and press Ctrl + Shift + C 
+to toggle multi-line comments.
+
+basic calculator
+1+1
+(5*3) / 2
+
+Modduro
+5%%2
+5%%5
+10%%3
+
+Number to the power of ...
+5**2
+3**2
+3**2; 3**3; 5**2
+
+Absolute Value 
+คือฟังก์ชันสำหรับเปลี่ยนค่าตัวเลขให้เ
+ป็นค่าบวกเสมอ (ระยะห่างจาก 0)ถ้าใส่
+เลขลบ  จะได้ เลขบ
+เลขบวก จะได้ ค่าเดิม
+abs(x) = ลบเครื่องหมายลบออ
+ให้เหลือแค่ค่าบวก
+
+x <- c(-10, 5, -3, 0)
+abs(x)     
+# ผลลัพธ์: 10  5  3  0
+
+-10* -1
+# ผลลัพธ์ 10
+
+abs(-99)
+# ผลลัพธ์
+99
+
+
+log(2)
+exp(10)
+
+
+
+
+
+
+
+
+
+```
